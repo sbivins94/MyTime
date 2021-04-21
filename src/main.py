b68ce93b2e94cf769caf3e6009dd0ed80b02e96a
sbivins94 <bivins.sean7@gmail.com>
2021-04-21 10:33:09 -0500

    Update main.py
    Base code from which Designer-Sublime transition can run

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 
-class MainWindow(QDialog):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        loadUi("", self)
+        super(QMainWindow, self).__init__()
+        loadUi("task-log_v02.ui",self)
 
-
+# main
+app = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
